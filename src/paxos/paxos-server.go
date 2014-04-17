b5b44c4f44f1896ce1f527c536d9dfe4ac5c1625
Soumya Kethu <skethu@andrew.cmu.edu>
2014-04-17 01:00:51 -0400

    wrote propose request and added stuff to the connections and rpc

--- src/paxos/paxos-server.go
+++ src/paxos/paxos-server.go
@@ -2,24 +2,17 @@ package paxos
 
 import (
 	"errors"
-	"net/rpc"
 	"net"
 	"strconv"
-	"net/http"
 	"time"
 	"sync"
+	"rpc"
+	"http"
+	"sync"
+	"net/rpc"
+	"net/http"
 )
 
-
-type MessageType int
-
-const (
-	ProposeRequest = iota + 1
-	ProposeResponse
-	AcceptRequest
-	AcceptResponse
-	Commit
-)
 
 type ProposeRequestArgs struct {
 	proposalID int
@@ -44,12 +37,6 @@ type CommitArgs struct {
 	value []byte
 }
 
-type PaxosMessage struct {
-	msgType     MessageType
-	proposalID  int
-	value       []byte
-}
-
 type PaxosServer struct {
 	//TODO
 	numNodes int
@@ -63,6 +50,8 @@ type PaxosServer struct {
 
 	serverRing PaxosRing
 	serverRingLock *sync.Mutex
+
+	paxosConnections map[int] *rpc.Client
 }
 
 type PaxosRing struct{
@@ -91,10 +80,13 @@ type GetServersReply struct {
 
 
 /*
- * creates a new paxos server node.
- * it connects on a port number (port),
- * and stores the current number of nodes (numNodes) so that it knows what the simple majority needed is supposed to be.
- * additionally, we require a masterHostPort
+ * Creates a new paxos server and starts the paxos server. masterServerHostPort is the
+ * master paxos server which is used to ensure all the paxos servers join the ring. If
+ * masterHostPort is empty then it is the master; otherwise, this server is a regular
+ * paxos server. port is the the port number that this server should listen on.
+ *
+ * This function should return only once all paxos servers have joined the ring
+ * and should return a non-nil error if the storage server could not be started
  *
  */
 func NewPaxosServer(masterHostPort string, numNodes, port int) (PaxosServer, error) {
@@ -127,44 +119,83 @@ func NewPaxosServer(masterHostPort string, numNodes, port int) (PaxosServer, err
 		err = paxosServer.startServer()
 	}
 
+	//dialing to all other paxos servers and storing the connection
+	for i:=0; i<numNodes; i++ {
+		currPort := paxosServer.serverRing.servers[i]
+		if currPort == port {
+			continue
+		} else {
+			serverConn, dialErr := rpc.DialHTTP("tcp", "localhost:"+currPort)
+			if dialErr != nil {
+				return paxosServer, dialErr
+			} else {
+				paxosServer.paxosConnections[currPort] = serverConn
+			}
+		}
+	}
+
 	return paxosServer, err
 }
 
-/*
- *
+
+/* RegisterServer adds a paxos server to the ring. It replies with an error if
+ * not all the paxos servers have joined. Once all servers have joined it
+ * returns a nil error and a list of all the ports the paxos servers are on.
  */
-func (ps *PaxosServer) RegisterServer(args *RegisterArgs, reply *RegisterReply) error {
+func (ps *PaxosServer) RegisterServer(args *RegisterArgs, reply *RegisterReply){
 
 	ps.serverRingLock.Lock()
 	for i:= 0; i< len(ps.serverRing.servers); i++ {
 		if(ps.serverRing.servers[i] == args.port) {
 			ps.serverRingLock.Unlock()
-			return errors.New("ALready Registered this server")
+			reply.err = errors.New("ALready Registered this server")
+			return
 		}
 	}
 
+	reply.err = nil
+	reply.servers = ps.serverRing.servers
 	ps.serverRing.servers[ps.serverRing.numConnected] = args.port
 	ps.serverRingLock.Unlock()
-	return nil
 }
 
-/*
- *
+
+/* GetServers retrieves a list of all connected paxos servers in the ring.
+ * It replies with ready equal to false if not all nodes have joined
+ * and otherwise with with ready equal to true and the server list
  */
-func (ps *PaxosServer) GetServers(args GetServersArgs, reply GetServersReply) error {
+func (ps *PaxosServer) GetServers(args GetServersArgs, reply GetServersReply){
 	if ps.serverRing.numConnected == ps.numNodes {
 		reply.servers = ps.serverRing.servers
 		reply.ready = true
 	} else {
 		reply.ready = false
 	}
+}
+
+/* Propose Request should make a rpc call to all the other paxos servers
+ * with a proposal ID.
+ */
+func (ps *PaxosServer) ProposeRequest() error{
+	if ps.recentPropNum > ps.lastPropNum {
+		ps.recentPropNum++
+	} else {
+		ps.recentPropNum = ps.lastPropNum + 1
+	}
+
+	requestArgs := ProposeRequestArgs{ps.recentPropNum}
+
+	for _, conn := range ps.paxosConnections {
+		conn.Call("PaxosServer.HandleProposeRequest", requestArgs)
+	}
+
 	return nil
 }
 
-/*
+/* 
  *
  */
-func (ps *PaxosServer) ProposeRequest() error{
+func (ps *PaxosServer) HandleProposeRequest() error{
 	//TODO
 	return errors.New("not implemented")
 }
@@ -172,7 +203,7 @@ func (ps *PaxosServer) ProposeRequest() error{
 /*
  *
  */
-func (ps *PaxosServer) HandleProposeRequest(msg PaxosMessage) error{
+func (ps *PaxosServer) HandleProposeResponse() error {
 	//TODO
 	return errors.New("not implemented")
 }
@@ -180,7 +211,7 @@ func (ps *PaxosServer) HandleProposeRequest(msg PaxosMessage) error{
 /*
  *
  */
-func (ps *PaxosServer) HandleProposeResponse(msg PaxosMessage) error {
+func (ps *PaxosServer) HandleAcceptRequest() error{
 	//TODO
 	return errors.New("not implemented")
 }
@@ -188,7 +219,7 @@ func (ps *PaxosServer) HandleProposeResponse(msg PaxosMessage) error {
 /*
  *
  */
-func (ps *PaxosServer) HandleAcceptRequest(msg PaxosMessage) error{
+func (ps *PaxosServer) HandleAcceptResponse() error{
 	//TODO
 	return errors.New("not implemented")
 }
@@ -196,15 +227,7 @@ func (ps *PaxosServer) HandleAcceptRequest(msg PaxosMessage) error{
 /*
  *
  */
-func (ps *PaxosServer) HandleAcceptResponse(msg PaxosMessage) error{
-	//TODO
-	return errors.New("not implemented")
-}
-
-/*
- *
- */
-func (ps *PaxosServer) HandleCommit(msg PaxosMessage) error{
+func (ps *PaxosServer) HandleCommit() error{
 	//TODO
 	return errors.New("not implemented")
 }
@@ -219,7 +242,7 @@ func (ps *PaxosServer) HandleCommit(msg PaxosMessage) error{
  */
 func (ps *PaxosServer) startMaster() error {
 
-	errRegister := rpc.RegisterName("StorageServer", ps)
+	errRegister := rpc.RegisterName("PaxosServer", ps)
 	rpc.HandleHTTP()
 
 	if errRegister != nil {
@@ -284,7 +307,7 @@ func (ps *PaxosServer) startServer() error {
 
 	for {
 		numTries++
-		args := &RegisterArgs{"localhost:" + strconv.Itoa(ss.port)}
+		args := &RegisterArgs{"localhost:" + strconv.Itoa(ps.port)}
 		var reply *storagerpc.RegisterReply
 
 		errCall := slave.Call("StorageServer.RegisterServer", args, &reply)
