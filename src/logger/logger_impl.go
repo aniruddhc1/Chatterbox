fd5e575f28777cf08838f1f34ff3eca73e77fcb0
Soumya Kethu <skethu@andrew.cmu.edu>
2014-04-18 16:18:11 -0400

    added boiler plate code for logging

---
+++ src/logger/logger_impl.go
@@ -0,0 +1,36 @@
+package logger
+
+
+import (
+	"errors"
+)
+
+
+type LogMessage struct {
+
+}
+
+type Logger struct{
+
+}
+
+func NewLogger() (*Logger, error) {
+	//TODO
+
+	return nil, errors.New("Unimplemented")
+}
+
+func (log *Logger) post() error {
+	//TODO
+	return nil
+}
+
+func (log *Logger) getLog() error {
+	//TODO
+	return nil
+}
+
+//TODO idk what else this file should have.. 
+
+
+
