4c56a8780b2097943d846181333a3407dcc9311d
Aniruddh Chaturvedi <aniruddhc@cmu.edu>
2014-04-25 21:14:14 -0400

    hi

--- src/github.com/cmu440/chatterbox/chatclient/chat-client.go
+++ src/github.com/cmu440/chatterbox/chatclient/chat-client.go
@@ -1,141 +1,113 @@
 package chatclient
-//
-//import (
-//	"errors"
-//	"net/rpc"
-//	"fmt"
-//	"net"
-//	"net/http"
-//	"container/list"
-//	"github.com/cmu440/chatterbox/multipaxos"
-//	"strconv"
-//	"code.google.com/p/go.net/websocket"
-//	"os"
-//	"bufio"
-//	"time"
-//	"github.com/cmu440/chatterbox"
-//)
-//
-//type User struct{ //wrapper struct for extensibility
-//	Username string
-//	Rooms *list.List
-//	ActiveRoom Room
-//	Connection *websocket.Conn
-//}
-//
-//type Room struct{ //wrapper struct for extensibility
-//	Name string
-//	Users *list.List
-//	//option to close the chat room?
-//}
-//
-//type ChatClient struct {
-//	ClientConn *rpc.Client
-//	Users *list.List //list of all users
-//	Rooms *list.List //list of all chat rooms
-//
-//}
-//
-//type InputArgs struct {
-//	ws *websocket.Conn
-//}
-//
-//type OutputArgs struct {
-//
-//}
-//
-//var currentRoom *Room = &Room{}
-//
-//func NewChatClient(hostport string, paxosPort int) (*ChatClient, error) {
-//	//TODO unimplemented
-//
-//	chatclient := &ChatClient{
-//		Users : list.New(),
-//		Rooms : list.New(),
-//	}
-//
-//	errRegister := rpc.RegisterName("ChatClient", Wrap(chatclient))
-//	if errRegister != nil {
-//		fmt.Println("Couldln' t register test chat client", errRegister)
-//		return nil, errRegister
-//	}
-//
-//	rpc.HandleHTTP()
-//	listener, errListen := net.Listen("tcp", hostport)
-//
-//	if errListen != nil {
-//		fmt.Println("Couldln't listen test chat client", errListen)
-//		return nil, errListen
-//	}
-//
-//	go http.Serve(listener, nil)
-//
-//	chatConn, errDial := rpc.DialHTTP("tcp", "localhost:"+strconv.Itoa(paxosPort))
-//	if errDial != nil {
-//		fmt.Println("Couldn't dialtest chat client", errDial)
-//		return nil, errDial
-//	}
-//
-//	chatclient.ClientConn = chatConn
-//
-//	return chatclient, nil
-//}
-//
-//func (cc *ChatClient) CreateNewUser(args *InputArgs, reply *OutputArgs) error { //needs user and room
-//	username := args.ws.Request().URL.Query().Get("username")
-//
-//	if username == ""{
-//		return errors.New("username is invalid")
-//	}
-//
-//	user := &User{
-//		Username : username,
-//		Connection : args.ws,
-//		ActiveRoom : currentRoom,
-//		Rooms : list.New().PushBack(currentRoom),
-//	}
-//
-//	currentRoom.Users.PushBack(user)
-//
-//	cc.UpdateView(&OutputArgs{}, &UpdateViewReply{})
-//
-//
-//
-//}
-//
-//
-//func NewChatRoom(){
-//	currentRoom := &Room{
-//		Name : "",
-//		Users : list.New(),
-//	}
-//	return currentRoom
-//}
-//
-//type UpdateViewReply struct{
-//	file os.File
-//}
-//
-//func (cc *ChatClient) UpdateView(args *OutputArgs, reply *UpdateViewReply){
-//	inputFile := bufio.NewScanner(reply.file)
-//	for {
-//		line := inputFile.ReadLine()
-//		fmt.Println(line)
-//		time.Sleep(time.Second)
-//	}
-//}
-//
-//func (cc *ChatClient) JoinChatRoom(args *InputArgs, reply *OutputArgs) error { //needs user and room
-//	return errors.New("Not Implemented")
-//}
-//
-//func (cc *ChatClient) SendMessage(args *multipaxos.SendMessageArgs, reply *multipaxos.SendMessageReplyArgs) error {
-//	fmt.Println("Sending Message in Chat Client")
-//	errCall := cc.ClientConn.Call("PaxosServer.SendMessage", &args, &reply)
-//	return errCall
-//}
-//
-//func (cc *ChatClient) GetServers(args *multipaxos.GetServersArgs, reply*multipaxos.GetServersReply) error {
-//	return cc.ClientConn.Call("PaxosServer.GetServers", &args, &reply)
-//}
+
+import (
+	"code.google.com/p/go.net/websocket"
+	"fmt"
+	"net/rpc"
+)
+
+type JoiningUser struct{
+	CurrRoom *CurrRoom
+	Connection *websocket.Conn
+	Name User
+	Send chan Message
+}
+
+type User struct{
+	Name string
+}
+
+type CurrRoom struct{
+	Users map[string]*JoiningUser
+	Broadcast chan Message
+
+}
+
+type Message struct{
+	User *User
+	Time string
+	Contents string
+}
+
+func StartChatRoom(){
+	activeRoom := &CurrRoom{
+		Users : make(map[string]*JoiningUser),
+		Broadcast : make(chan Message),
+	}
+	go activeRoom.Run()
+}
+
+func (chatRoom *CurrRoom) Run(){
+	for{
+		select{
+		case msg := <-chatRoom.Broadcast:
+			for _, user := chatRoom.Users{
+				user.Send <- msg
+			}
+		//close case
+		}
+	}
+}
+
+var activeRoom *CurrRoom = &CurrRoom{}
+
+func StartConnection(ws *websocket.Conn){
+	username := ws.Request().URL.Query().Get("username")
+
+	if username == "" {
+		return
+	}
+
+	joiningUser := &JoiningUser{
+		CurrRoom: activeRoom,
+		Connection : ws,
+		Name : username,
+	}
+
+	activeRoom.Users[username] = joiningUser
+
+	go joiningUser.ToClient()
+
+	joiningUser.FromClient()
+
+	//stop chatroom
+
+}
+
+func (user *JoiningUser) ToClient(){
+
+	//simple rpc call to sendMessage in paxos server
+
+
+}
+
+func (user *JoiningUser) FromClient(){
+
+	//make rpc call to paxos server, read the file here
+
+	for{
+		var message string
+
+		if err != nil{
+			fmt.Println(err)
+		}
+
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/github.com/cmu440/chatterbox/chatclient/main.go
+++
@@ -1,9 +0,0 @@
-package chatclient
-
-import "github.com/cmu440/chatterbox/chatclient"
-
-func main(){
-
-	server := chatclient.
-
-}
